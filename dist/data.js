7f6c44e8f422882992f79af8572e57884b6b656a
fhrrrzy <fahrurrozy4214@gmail.com>
2023-05-24 16:26:43 +0700

    Remove node_modules from Git

---
+++ dist/data.js
@@ -0,0 +1,34 @@
+export const dataProducts = [
+    {
+        "id":1,
+        "name" : "Taplak Meja Sulam",
+        "image" : "taplak.jpg",
+        "price" : "Rp. 55.000",
+        "description" : "Taplak Meja Sulam MTS NIB adalah produk unggulan dalam penjualan taplak meja yang berkualitas tinggi. Dibuat dengan keahlian sulam yang indah dan detail, taplak meja ini akan memberikan sentuhan elegan dan mewah pada meja makan Anda. Dengan berbagai desain dan pola yang menarik, Taplak Meja Sulam MTS NIB cocok untuk berbagai acara dan kesempatan, baik itu makan malam formal maupun santai. Bahan berkualitas tinggi yang digunakan dalam pembuatannya membuatnya tahan lama dan mudah dirawat. Dengan Taplak Meja Sulam MTS NIB, Anda dapat meningkatkan tampilan ruang makan Anda dengan sentuhan gaya yang elegan dan mengundang pujian dari tamu-tamu Anda.",
+        "crafted_by" : "KELAS IX MTS SWASTA NURUL IMAN"
+    },
+    {
+        "id":2,
+        "name" : "Baju Adat Sulam",
+        "image" : "baju.jpg",
+        "price" : "Rp. 1.200.000",
+        "description" : "Baju Adat Sulam MTS NIB adalah koleksi baju adat yang memukau dengan sulaman tradisional yang indah dan detail. Setiap potongan baju adat ini dibuat dengan keahlian tinggi dan menggunakan kain berkualitas terbaik. Sulaman yang rumit dan cantik menambah keanggunan pada baju adat ini, menciptakan penampilan yang anggun dan memikat. Baju Adat Sulam MTS NIB tersedia dalam berbagai gaya dan warna, yang cocok untuk acara-acara istimewa seperti pernikahan, upacara tradisional, atau acara budaya lainnya. Dengan mengenakan Baju Adat Sulam MTS NIB, Anda akan merasakan keanggunan budaya dalam setiap langkah Anda dan memikat semua mata yang memandang. ",
+        "crafted_by" : "KELAS IX MTS SWASTA NURUL IMAN"
+    },
+    {
+        "id":3,
+        "name" : "KARBOL MTS NIB",
+        "image" : "karbol.jpg",
+        "price" : "Rp. 10.000",
+        "description" : "Karbol MTS NIB adalah merek terpercaya dalam penjualan produk pembersih Karbol. Dikenal karena kualitasnya yang unggul, Karbol MTS NIB merupakan solusi terbaik untuk membersihkan dan melindungi rumah Anda dari kuman dan bakteri. Produk Karbol MTS NIB mengandung bahan aktif yang efektif dalam membunuh kuman, menghilangkan bau tak sedap, dan menjaga kebersihan lingkungan sekitar. Dengan kemasan yang praktis dan mudah digunakan, Karbol MTS NIB memberikan kebersihan dan perlindungan yang optimal bagi rumah Anda, membuatnya menjadi pilihan terbaik bagi mereka yang mengutamakan kebersihan dan kesehatan keluarga. ",
+        "crafted_by" : "KELAS VIII MTS SWASTA NURUL IMAN"
+    },
+    {
+        "id":4,
+        "name" : "SABUN CUCI PIRING MTS NIB",
+        "image" : "sabun.jpg",
+        "price" : "Rp. 7.000",
+        "description" : "Sabun cuci piring MTS NIB merupakan merek terkemuka dalam penjualan produk sabun cuci piring. Sabun cuci piring MTS NIB memiliki formulasi khusus yang kuat dalam menghilangkan lemak dan kotoran yang menempel pada peralatan dapur. Dengan kemasan praktis dan mudah digunakan, sabun cuci piring MTS NIB memberikan kebersihan yang maksimal dan menjaga peralatan dapur tetap bersih dan bebas kuman. Produk ini juga terkenal karena keamanannya bagi lingkungan dan ramah terhadap kulit, membuatnya menjadi pilihan yang tepat untuk rumah tangga yang peduli akan kebersihan dan kelestarian lingkungan. ",
+        "crafted_by" : "KELAS VIII MTS SWASTA NURUL IMAN"
+    }
+]
